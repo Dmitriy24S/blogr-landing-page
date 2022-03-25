9b1bc3e02f730cdb102a90ac1f8f47325b976d34
Dmitriy <devilins24@gmail.com>
2022-03-25 18:26:00 +0200

    desktop nav show submenu on hover, show submenu on tabover, add/remove event listeners on window resize

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 const menuBtn = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
-
 const menuHeadersText = document.querySelectorAll(".list-header-text-wrap");
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
+function headerClick(e) {
+  const headerMainElement = e.currentTarget.parentNode;
+  headerMainElement.classList.toggle("show");
+  document.activeElement.blur();
+}
 
-menuHeadersText.forEach((headerText) => {
-  headerText.addEventListener("click", () => {
-    const headerMainElement = headerText.parentNode;
-    headerMainElement.classList.toggle("show");
-  });
-});
+function addShowtomenu() {
+  menu.classList.toggle("show");
+}
+
+let mobileNavEventListeners = false;
+
+const mobileNav = () => {
+  // if mobile width resize
+  if (window.innerWidth <= 1078) {
+    if (mobileNavEventListeners === false) {
+      mobileNavEventListeners = true;
+      // [...menuHeadersText].forEach((headerText) => {
+      menuHeadersText.forEach((headerText) => {
+        headerText.addEventListener("click", headerClick);
+      });
+      menuBtn.addEventListener("click", addShowtomenu);
+    } else {
+      return;
+    }
+  } else {
+    // if desktop width resize
+    if (mobileNavEventListeners === true) {
+      mobileNavEventListeners = false;
+      // [...menuHeadersText].forEach((headerText) => {
+      menuHeadersText.forEach((headerText) => {
+        const headerMainElement = headerText.parentNode;
+        headerMainElement.classList.remove("show");
+        headerText.removeEventListener("click", headerClick);
+      });
+      menu.classList.remove("show");
+      menuBtn.removeEventListener("click", addShowtomenu);
+    } else {
+      return;
+    }
+  }
+};
+
+mobileNav();
+window.onresize = mobileNav;
